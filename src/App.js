a46190ae0aecb458afa2e5aa7a481ab823a21ee2
NavarroErk <navarroerk@gmail.com>
2023-06-17 14:19:40 -0700

    test commit after linking gh-pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TEST TEXT HERE
         </a>
       </header>
     </div>
